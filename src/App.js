3da57675fbe2ca75ca8902b5eeff04f64cfd8946
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-11-13 21:38:43 +0900

    chore: 데이터 등록 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
 import TodoList from './component/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정관리 앱 만들어보기',
+      checked: false,
+    },
+  ]);
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
