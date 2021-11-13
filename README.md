4adf84e1b29ff5570af3511bab3965bb5d3e3a1e
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-11-13 17:18:41 +0900

    feat: Create TodoTemplate & TodoInsert Component

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Make ToDo App with React, Sass, classname, react-icon
+
+react-icons  
+<https://react-icons.github.io/react-icons/#/icons/md>
--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import TodoTemplate from './component/TodoTemplate';
+import TodoInsert from './component/TodoInsert';
 
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
 };
 
 export default App;
